2206eb6ab48ebb7a7427940d254e566440827b26
Rozitta <rozitta2009@gmail.com>
2017-10-02 16:27:33 +0300

    решение первой задачи вторым способом

--- JS_if operatop/IF1.js
+++ JS_if operatop/IF1.js
@@ -1,10 +1,16 @@
 // Дано целое число. Если оно является положительным, то прибавить к нему 1; 
 // в противном случае не изменять его. Вывести полученное число.
 
-var number = prompt("Введите число");
+// 1 вариант решения с помошью if
+    var number = prompt("Введите число");
 
-if(number >= 0){
-    alert(+number+1);
-}else{
-    alert(+number);
-}
+    if(number >= 0){
+        alert(+number+1);
+    }else{
+        alert(+number);
+    }
+
+// 2 вариант решения с помошью тернарного оператора
+    var number = prompt("Введите число");
+    number = (number >= 0) ? (+number+1) : (+number);
+    alert(number);
